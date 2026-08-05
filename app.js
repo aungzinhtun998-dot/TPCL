4b6565ee99e1bec2093ae993786d7106cdf086ce
aungzinhtun998-dot <aungzinhtun998@gmail.com>
2026-08-05 19:55:38 +0630

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,25 @@
-console.log("TPCL v3.0 Started");
+const map = L.map("map").setView([16.8661, 96.1951], 7);
 
-if ("serviceWorker" in navigator) {
-    navigator.serviceWorker.register("service-worker.js");
+L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    maxZoom:19,
+    attribution:"© OpenStreetMap"
+}).addTo(map);
+
+// User GPS
+if(navigator.geolocation){
+
+    navigator.geolocation.getCurrentPosition(function(position){
+
+        const lat = position.coords.latitude;
+        const lng = position.coords.longitude;
+
+        L.marker([lat,lng])
+            .addTo(map)
+            .bindPopup("📍 Your Location")
+            .openPopup();
+
+        map.setView([lat,lng],12);
+
+    });
+
 }
